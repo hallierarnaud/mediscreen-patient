46088456407d073a207571a7f2f2319ab7778d48
hallier <ahallier@isara.fr>
2021-12-23 12:31:23 +0100

    Fix : CORS add @CrossOrigin annotation to PatientController.java

--- src/main/java/com/openclassrooms/mediscreen/controller/endpoint/PatientController.java
+++ src/main/java/com/openclassrooms/mediscreen/controller/endpoint/PatientController.java
@@ -6,6 +6,7 @@ import com.openclassrooms.mediscreen.domain.service.PatientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.NoSuchElementException;
 
 import javax.persistence.EntityExistsException;
 
+@CrossOrigin(origins="*")
 @RestController
 public class PatientController {
 
